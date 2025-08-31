4074cd67b93eaecb3c0e1ef2ad7385a8ad396850
HITHESH2023 <149927521+HITHESH2023@users.noreply.github.com>
2025-08-31 23:03:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "HPCG-Portfolio-Website",  // <-- replace with your GitHub repo name
+  base: "HPCG-Portfolio-Website",
 })
